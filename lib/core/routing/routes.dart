c0b33bac6a340d1c923f91c6ccaaec063db2627b
Eslam <xdev.eslam@gmail.com>
2026-07-13 22:56:19 +0300

    routes

--- lib/core/routing/routes.dart
+++ lib/core/routing/routes.dart
@@ -1,7 +1,6 @@
 class Routes {
-  static const String loginScreen = 'lib/features/auth/ui/login_screen.dart';
-  static const String signupScreen = 'lib/features/auth/ui/singup_screen.dart';
-  static const String otpScreen = 'lib/features/auth/ui/otp.dart';
-  static const String profileScreen =
-      'lib/features/profile/ui/profile_screen.dart';
+  static const String loginScreen = '/login';
+  static const String signupScreen = '/signup';
+  static const String otpScreen = '/otp';
+  static const String profileScreen = '/profile';
 }
